16aaaf726c5bc2834904e00b5357357492eec0f6
Carlos-RSCH <carlosr3425@gmail.com>
2025-12-02 16:47:28 -0600

    Corrigiendo base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/agenda-frontend/',
+  base: '/agenda-frontend2/',   
   plugins: [vue()]
 })
